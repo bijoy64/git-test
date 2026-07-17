8461a776412f42f1fa5f040da8c865de48c256d0
bijoy64 <bijoykuri29@gmail.com>
2026-07-17 15:09:41 +0600

    Add files via upload

---
+++ largest number.py
@@ -0,0 +1,9 @@
+def largest():
+    number1=int(input("Enter the value of 1stnumber1: "))
+    number2=int(input("Enter the value of 2ndnumber2: "))
+    if number1>number2:
+        print("The largest number is:",number1)
+    else:
+        print("The largest number is:",number2)
+
+largest()
